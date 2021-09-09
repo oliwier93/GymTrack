b6fbefd8d8e0c7fadef2936556b7449a8e636dd2
Oliwier <oliwier.baran@icloud.com>
2021-09-09 15:38:20 +0200

    Add initial first feature - add workout

---
+++ gym-track-app/src/app/features/add-activity/add-activity.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddActivityComponent } from './add-activity.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddActivityComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddActivityModule { }
