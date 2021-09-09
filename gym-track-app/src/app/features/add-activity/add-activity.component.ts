7d86492c365d75114ab43e76fe9317ce7c5107b8
Oliwier <oliwier.baran@icloud.com>
2021-09-09 15:46:21 +0200

    Fix app name and prefix

--- gym-track-app/src/app/features/add-activity/add-activity.component.ts
+++ gym-track-app/src/app/features/add-activity/add-activity.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-activity',
+  selector: 'gta-add-activity',
   templateUrl: './add-activity.component.html',
   styleUrls: ['./add-activity.component.scss']
 })
