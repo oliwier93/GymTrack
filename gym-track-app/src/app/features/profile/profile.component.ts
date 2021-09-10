df952316adc7679367a73fa0493cb84d2d5a1f60
Oliwier <oliwier.baran@icloud.com>
2021-09-10 13:24:27 +0200

    Initial title bar to profile component

--- gym-track-app/src/app/features/profile/profile.component.ts
+++ gym-track-app/src/app/features/profile/profile.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuElement {
+  value: string;
+  viewValue: string;
+  icon: string;
+  isVisible: boolean;
+}
+
 @Component({
   selector: 'gta-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  menuElements: MenuElement[] = [
+    {value: 'measurements', viewValue: 'Measurements', icon:'straighten', isVisible: true},
+    {value: 'settings', viewValue: 'Settings', icon:'settings', isVisible: true}
+  ]
 
   constructor() { }
 
--- gym-track-app/src/app/features/profile/profile.module.ts
+++ gym-track-app/src/app/features/profile/profile.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { APP_BASE_HREF, CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -9,7 +12,10 @@ import { ProfileComponent } from './profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     ProfileComponent
