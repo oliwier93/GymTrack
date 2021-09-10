669324c818e9040a17103a10f852e20f7da70abb
Oliwier <oliwier.baran@icloud.com>
2021-09-10 11:21:20 +0200

    Initial commit of all features

---
+++ gym-track-app/src/app/features/exercises/exercises.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gta-exercises',
+  templateUrl: './exercises.component.html',
+  styleUrls: ['./exercises.component.scss']
+})
+export class ExercisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ gym-track-app/src/app/features/upgrade/upgrade.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UpgradeComponent } from './upgrade.component';
+
+
+
+@NgModule({
+  declarations: [
+    UpgradeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UpgradeModule { }
